ee5c2d20cf2af72956800d429b6c96a76aeffb58
Tejbang123 <bangtejshree123@gmail.com>
2023-09-11 22:37:38 +0530

    Initial commit

---
+++ packages/marketing/config/webpack.common.js
@@ -0,0 +1,17 @@
+module.exports={
+    module:{
+        rules:[
+            {
+                test:/\.m?js$/,                 //file end wit .m or .js, it will load wit babel
+                exclude: /node_modules/,
+                use:{
+                    loader:'babel-loader',
+                    options:{
+                        presets:['@babel/preset-react','@babel/preset-env'],
+                        plugins:['@babel/plugin-transform-runtime']
+                    }
+                }
+            }
+        ]
+    }
+}
